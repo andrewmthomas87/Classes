4482f52bc56c33d988953b17c962ea1e4bff90a8
Andrew Thomas <andrewmthomas87@gmail.com>
2015-02-01 22:10:16 -0700

    Added additional files

--- js/home.js
+++ js/home.js
@@ -4,7 +4,7 @@ var position = 0;
 document.addEventListener('deviceready', function() {
 	$('nav a').click(function() {
 		position = position ? 0 : 1;
-		$('section').css('left', position ? '-100%' : '0');
+		$('section>div').css('left', position ? '-100%' : '0');
 		$('section>div:nth-child(' + (position + 1) + ')').css('height', 'auto');
 		$('section>div:nth-child(' + (2 - position) + ')').css('height', '0');
 		$('nav span').fadeOut(125, function() {
