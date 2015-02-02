56be1b80d8a753ad7dc932614a78d2e589b57286
Andrew Thomas <andrewmthomas87@gmail.com>
2015-02-01 22:54:04 -0700

    Fixed storage

--- js/home.js
+++ js/home.js
@@ -2,8 +2,8 @@
 var position = 0;
 
 document.addEventListener('deviceready', function() {
-	for (i = 0; i < 8; i++) {
-		$('section div div textarea#' + (i + 1)).val(window.localStorage.getItem('block-' + (i + 1)));
+	for (i = 1; i < 9; i++) {
+		$('section div div textarea#' + i).val(window.localStorage.getItem('block-' + i));
 	}
 	$('section div div textarea').elastic();
 	$('section div div textarea').focus(function() {
@@ -13,7 +13,7 @@ document.addEventListener('deviceready', function() {
 	$('section div div textarea').blur(function() {
 		$('nav').fadeIn(125);
 		$('section').css('padding-top', '4.625em');
-		window.localStorage.setItem('block-' + (parseInt($(this).attr('id')) - 1), $(this).val());
+		window.localStorage.setItem('block-' + parseInt($(this).attr('id')), $(this).val());
 	});
 	$('nav a').click(function() {
 		position = position ? 0 : 1;
