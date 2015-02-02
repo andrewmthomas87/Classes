1088bed8ce849b5b4fc829c0ca8311df4b3c37c0
Andrew Thomas <andrewmthomas87@gmail.com>
2015-02-01 21:57:21 -0700

    Attempted to fix nav overlay

--- js/home.js
+++ js/home.js
@@ -4,9 +4,9 @@ var position = 0;
 document.addEventListener('deviceready', function() {
 	$('nav a').click(function() {
 		position = position ? 0 : 1;
-		$('section div:nth-child(' + (position + 1) + ')').css('height', 'auto');
-		$('section div:nth-child(' + (2 - position) + ')').css('height', '0');
-		$('section div').css('left', position ? '-100%' : '0');
+		$('section>div:nth-child(' + (position + 1) + ')').css('height', 'auto');
+		$('section>div:nth-child(' + (2 - position) + ')').css('height', '0');
+		$('section>div').css('left', position ? '-100%' : '0');
 		$('nav span').fadeOut(125, function() {
 			$(this).html('Day ' + (position + 1));
 			$(this).fadeIn(125);
